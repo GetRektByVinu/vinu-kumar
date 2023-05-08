fa4cb8f8f70f9c59d700258cd377c114c5858d96
Vinu <2172219@talnet.nl>
2023-05-08 11:22:44 +0200

    first commit

---
+++ php-op1/test.php
@@ -0,0 +1,23 @@
+<?php 
+
+// onderdeel 1
+// $getal1= 12;
+// $getal2= 10;
+// echo "Het resultaat is" . $getal1 + $getal2;
+
+
+// onderdeel 2
+// $getal1= 12;
+// $getal2= 10;
+// echo "Het resultaat is" . $getal1 * $getal2;
+
+
+// onderdeel 3
+// echo "Welkom Vinu"
+
+
+$Naam1="Carl";
+$Naam2="Camel";
+echo $Naam1 . " zit bij " . $Naam2 . " in de klas!";
+
+?>
